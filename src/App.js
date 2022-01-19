c9319f2e9133b5f436aba8146b0186c90d1d6e50
unknown <m.indraali105@gmail.com>
2022-01-20 05:48:47 +0800

    navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
